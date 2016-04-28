de9e0368c557a95c91409acf2b38aac9e88ec3a1
gruiton <julen.ormaetxea81@gmail.com>
2016-04-28 11:48:17 +0200

    Oracle to MySql

--- MyPlugins.py
+++ MyPlugins.py
@@ -1,16 +1,15 @@
 import sublime, sublime_plugin  
-class valuesCommand(sublime_plugin.TextCommand):
+
+class valuesCommand(sublime_plugin.TextCommand):	
 	def run(self, edit):
-		print("START")
 		self.documentFormatter(edit)
 		print(self.insertQuotes(edit))
-		print("FINISHED")
 	def insertQuotes(self, edit):
 		result = ""
 		markers = []
 		selections = self.view.find_all("^(.*)\n?", 0, "$1", markers)
-		selections = zip(*[iter(selections)] * 19)
-		markers = zip(*[iter(markers)] * 19)
+		selections = zip(*[iter(selections)] * 5)
+		markers = zip(*[iter(markers)] * 5)
 		for x in range(0, len(selections)):
 			selection_row = selections[x]
 			result += "("
@@ -34,13 +33,12 @@ class valuesCommand(sublime_plugin.TextCommand):
 		selection = self.view.find_all("^\n")
 		for region in reversed(selection):
 			self.view.replace(edit, region, "")
+
 class createCommand(sublime_plugin.TextCommand):			
 	def run(self, edit):
-		print("START")
 		self.containerFormatter(edit)
 		self.splitLines(edit)
 		self.termsFormatter(edit)
-		print("FINISHED")
 	def containerFormatter(self, edit):
 		markers = []
 		selection = self.view.find_all("^create table (.*?) \(", 0, "create table `smc_portal`.`$1`\n(", markers)
@@ -56,6 +54,41 @@ class createCommand(sublime_plugin.TextCommand):
 			replacement_text = markers[x]
 			self.view.replace(edit, region, replacement_text)
 	def termsFormatter(self, edit):
-		selection = self.view.find_all("VARCHAR2")
+		markers = []
+		#VARCHAR2
+		selection = self.view.find_all("(\s)+VARCHAR2\((\s*)?(.*?)(\s*)?\)", 0, "$3", markers)
+		for x in reversed(range(0, len(selection))):
+			region = selection[x]
+			marker = markers[x]
+			if(int(marker) > 1499):
+				replacement_text = " LONGTEXT"
+			else:
+				replacement_text = " VARCHAR(" + marker + ")"
+			print(self.view.substr(region) + " - " + marker)
+			self.view.replace(edit, region, replacement_text)
+		#CHAR
+		selection = self.view.find_all("(\s)+CHAR\((\s*)?(.*?)(\s*)?\)", 0, "$3", markers)
+		for x in reversed(range(0, len(selection))):
+			region = selection[x]
+			marker = markers[x]
+			if(int(marker) > 1499):
+				replacement_text = " LONGTEXT"
+			else:
+				replacement_text = " VARCHAR(" + marker + ")"
+			print(self.view.substr(region) + " - " + marker)
+			self.view.replace(edit, region, replacement_text)
+		#NUMBER
+		selection = self.view.find_all("(\s)+NUMBER")
 		for region in reversed(selection):
-			self.view.replace(edit, region, "VARCHAR")
+			print(str(region))
+			self.view.replace(edit, region, " INT")
+		#CLOB
+		selection = self.view.find_all("(\s)+CLOB\((\s*)?(.*?)(\s*)?\)")
+		for region in reversed(selection):
+			print(str(region))
+			self.view.replace(edit, region, " LONGTEXT")	
+		#DATE
+		selection = self.view.find_all("(\s)+DATE(\s*)?\((\s*)?(.*?)(\s*)?\)")
+		for region in reversed(selection):
+			print(str(region))
+			self.view.replace(edit, region, " DATE")										
